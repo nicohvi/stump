eb969b353f5a2b44430f72414e1d019e727fceed
Nicolay Hvidsten <nico.hvi@gmail.com>
2014-02-24 16:28:30 +0100

    restructure

---
+++ lib/config.rb
@@ -0,0 +1,29 @@
+require 'logger'
+require 'logger_targets'
+
+module Stump
+
+  class Config
+    def init(options)
+      log_path = options[:path]
+      if  FileTest.exist?(log_path)
+        log_file = open_logger(log_path)
+      else
+        log_file = create_logger(log_path)
+      end
+
+       Logger.new LoggerTargets.new(STDOUT, log_file)
+
+    end
+
+    private
+
+    def open_logger(file_path)
+      File.open(log_path, 'a')
+    end
+
+    def create_logger(file_path)
+      File.new(log_path)
+    end
+  end
+end
