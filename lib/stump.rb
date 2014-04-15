a2aa274490b7fc38db3079afed068c9176046186
Nicolay Hvidsten <nico.hvi@gmail.com>
2014-04-15 15:39:27 +0200

    refactoring

--- lib/stump.rb
+++ lib/stump.rb
@@ -1,7 +1,25 @@
 require 'stump/logger_targets'
-require 'stump/stump'
-require 'stump/stump_logger'
+# require 'stump/stump'
+# require 'stump/stump_logger'
 
 module Stump
+
+  p "fuck the world"
+
+  class Logger
+
+    def self.new(path = {})
+      return Logger.new(LoggerTargets.new(STDOUT)) unless path
+
+      if FileTest.exist?(path)
+        log_file = File.open(log_path, 'a')
+      else
+        FileUtils.mkdir_p(File.dirname(log_path))
+        log_file = File.new(log_path, 'w')
+      end
+       Logger.new(LoggerTargets.new(STDOUT, log_file), shift_age)
+    end
+
+end
 
 end
--- lib/stump/stump.rb
+++ lib/stump/stump.rb
@@ -7,6 +7,7 @@ module Stump
   # Instantiates a new Logger and provides a LoggerTargets object as the logger's LogDevice.
   # The LoggerTargets object can write to several targets in contrast to a regular device.
   #
+  class Stump
     def self.new(path = {})
       return Logger.new(LoggerTargets.new(STDOUT)) unless path
 
@@ -16,7 +17,6 @@ module Stump
         FileUtils.mkdir_p(File.dirname(log_path))
         log_file = File.new(log_path, 'w')
       end
-
        Logger.new(LoggerTargets.new(STDOUT, log_file), shift_age)
     end
 
@@ -24,4 +24,5 @@ module Stump
 
     end
 
+  end
 end
