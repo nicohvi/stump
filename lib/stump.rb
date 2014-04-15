878a1313a6a8ceaa6ab081b692c800c7f6abb31f
Nicolay Hvidsten <nico.hvi@gmail.com>
2014-04-15 15:42:36 +0200

    refactoring

--- lib/stump.rb
+++ lib/stump.rb
@@ -1,24 +1,27 @@
-require 'stump/logger_targets'
+# require 'stump/logger_targets'
 # require 'stump/stump'
 # require 'stump/stump_logger'
 
 module Stump
 
-  p "fuck the world"
+  # class Logger
 
-  class Logger
-
-    def self.new(path = {})
-      return Logger.new(LoggerTargets.new(STDOUT)) unless path
-
-      if FileTest.exist?(path)
-        log_file = File.open(log_path, 'a')
-      else
-        FileUtils.mkdir_p(File.dirname(log_path))
-        log_file = File.new(log_path, 'w')
-      end
-       Logger.new(LoggerTargets.new(STDOUT, log_file), shift_age)
-    end
+  def self.fuck
+    p "this sucks"
+  end
+  regjnlvnc.-mw
+  #
+  #   def self.new(path = {})
+  #     return Logger.new(LoggerTargets.new(STDOUT)) unless path
+  #
+  #     if FileTest.exist?(path)
+  #       log_file = File.open(log_path, 'a')
+  #     else
+  #       FileUtils.mkdir_p(File.dirname(log_path))
+  #       log_file = File.new(log_path, 'w')
+  #     end
+  #      Logger.new(LoggerTargets.new(STDOUT, log_file), shift_age)
+  #   end
 
 end
 
